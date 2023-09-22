9836362b4785c316906435d12a0309aa76ffa73f
axelt <r0784549@student.thomasmore.be>
2023-09-22 17:22:43 +0200

    6.2 styling

--- src/components/MenuProduct.js
+++ src/components/MenuProduct.js
@@ -1,7 +1,9 @@
-
 export function MenuProduct(props) {
     const {product} = props;
     const size = product.size ? "(" + product.size + "cl)" : "";
     if (!product?.name) return;
-    return <div> {product.name} {size} {product.price} &euro; </div>;
+    return <div style={{margin: "2vw 0", fontSize: "smaller", display: "flex", justifyContent: "space-between", width: "200px"}}>
+        <div>{product.name} {size} </div>
+        <div>{product.price} &euro; </div>
+    </div>;
 }
