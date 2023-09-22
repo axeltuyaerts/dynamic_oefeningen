86294acaf7892a2c674310705b295a8f944e2f3f
axelt <r0784549@student.thomasmore.be>
2023-09-22 18:04:08 +0200

    6.3 geneste Component

--- src/components/MenuProduct.js
+++ src/components/MenuProduct.js
@@ -1,9 +1,26 @@
-export function MenuProduct(props) {
+import * as PropTypes from "prop-types";
+
+function ProductSize(props) {
     const {product} = props;
     const size = product.size ? "(" + product.size + "cl)" : "";
+
+    return <span style={{color: "blue"}}>{size}</span>;
+}
+
+ProductSize.propTypes = {size: PropTypes.string};
+
+export function MenuProduct(props) {
+    const {product} = props;
     if (!product?.name) return;
-    return <div style={{margin: "2vw 0", fontSize: "smaller", display: "flex", justifyContent: "space-between", width: "200px"}}>
-        <div>{product.name} {size} </div>
+    return <div style={{
+        margin: "2vw 0",
+        fontSize: "smaller",
+        display: "flex",
+        justifyContent: "space-between",
+        width: "200px",
+        flex: "1"
+    }}>
+        <div>{product.name} <ProductSize product={product}/></div>
         <div>{product.price} &euro; </div>
     </div>;
 }
