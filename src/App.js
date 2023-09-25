5ff9024bf00641ffccbdbbaa7a7b04c605a111f8
Axel <r0784549@student.thomasmore.be>
2023-09-25 20:29:09 +0200

    2.2 De images weergeven

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ function App() {
                     <PicturesPage/>
                 </TabPanel>
             </Tabs>
+
+
         </div>
     );
 }
--- src/pages/PicturesPage.js
+++ src/pages/PicturesPage.js
@@ -1,7 +1,43 @@
+const PICTURES_DATA = [
+    {
+        id: 1,
+        credit: "https://unsplash.com/@acharki95?utm_source=unsplash&utm_medium=referral&utm_content = creditCopyText",
+        author: "Aziz Acharki",
+        name: "aziz-acharki-oEibQEiq2cM-unsplash.jpg"
+    },
+    {
+        id: 2,
+        credit: "https://unsplash.com/@acharki95?utm_source=unsplash&utm_medium=referral&utm_content = creditCopyText",
+        author: "Aziz Acharki",
+        name: "aziz-acharki-OJjVVwZVYuo-unsplash.jpg"
+    },
+    {
+        id: 3,
+        credit: "https://unsplash.com/@yan_slg?utm_source=unsplash&utm_medium=referral&utm_content = creditCopyText",
+        author: "Aziz Acharki",
+        name: "aziz-acharki-S53UjsK3A2o-unsplash.jpg"
+    }
+
+];
+
+
 export function PicturesPage() {
     return (
         <div>
             <h1>Pictures</h1>
+            {PICTURES_DATA.map(picture => <Picture key={picture.name} picture={picture}/>)}
+
         </div>
     )
+}
+
+function Picture(props) {
+    const {picture} = props;
+    return <div>
+        <img src={`images/` + picture.name}
+             alt={picture.name}
+             width="100%"
+             style={{margin: "2vw auto 0 auto"}}/>
+        By <a href={picture.credit}> {picture.author}</a>
+    </div>
 }
