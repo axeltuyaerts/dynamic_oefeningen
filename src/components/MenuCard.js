d692605ac41dca1c9f9166001bc5eeff0269128c
axelt <r0784549@student.thomasmore.be>
2023-09-26 12:06:34 +0200

    4.3 Bootstrap toepassen

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,13 +1,20 @@
 import {MenuProduct} from "./MenuProduct";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export function MenuCard(props) {
     const {products} = props;
     if (!products) return;
     return (
-        <div>
-            <h1>Menu</h1>
-            {products.map(p => <MenuProduct key={p.name} product={p}/>)}
-        </div>
+        <Container fluid>
+            <Row>
+                <Col >
+                    <h1>Menu</h1>
+                    {products.map(p => <MenuProduct key={p.name} product={p}/>)}
+                </Col>
+            </Row>
+        </Container>
 
     );
 }
