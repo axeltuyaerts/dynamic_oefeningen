60b8c0eacf1d23e48188b0105b7bf089b85a5b90
Axel <r0784549@student.thomasmore.be>
2023-09-26 23:45:17 +0200

    Bundel 3 2.1 Numbers component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Tab, Tabs, TabList, TabPanel} from "react-tabs";
 import 'react-tabs/style/react-tabs.css';
 import './App.css';
-import {PRODUCTS_DATA} from "./data/data";
+import {NUMBER_DATA, PRODUCTS_DATA} from "./data/data";
 import {MenuCardPage} from "./pages/MenuCardPage";
 import {PicturesPage} from "./pages/PicturesPage";
+import {Tab, Tabs, TabList, TabPanel} from "react-tabs";
+import {NumbersPage} from "./pages/NumbersPage";
 
 
 function App() {
     return (
-        <div id="root">
-            <Tabs>
-                <TabList>
-                    <Tab>menu</Tab>
-                    <Tab>pics</Tab>
-                </TabList>
+        <Tabs>
+            <TabList>
+                <Tab>menu</Tab>
+                <Tab>pics</Tab>
+                <Tab>numbers</Tab>
+            </TabList>
 
-                <TabPanel>
-                    <MenuCardPage products={PRODUCTS_DATA}/>
-                </TabPanel>
-                <TabPanel>
-                    <PicturesPage/>
-                </TabPanel>
-            </Tabs>
+            <TabPanel>
+                <MenuCardPage products={PRODUCTS_DATA}/>
+            </TabPanel>
 
+            <TabPanel>
+                <PicturesPage/>
+            </TabPanel>
 
-        </div>
+            <TabPanel>
+                <NumbersPage numbers={NUMBER_DATA}/>
+            </TabPanel>
+        </Tabs>
     );
 }
 
