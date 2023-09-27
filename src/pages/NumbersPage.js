ff7e73bb4444822ac9267305147b98e5de3f03cc
axelt <r0784549@student.thomasmore.be>
2023-09-27 18:22:12 +0200

    Bundel 3 2.2 numbers groter dan 6

--- src/pages/NumbersPage.js
+++ src/pages/NumbersPage.js
@@ -2,9 +2,11 @@ import {Numbers} from "../components/Numbers";
 
 export function NumbersPage(props) {
     const {numbers} = props
+
     return (
         <div>
             <Numbers numbers={numbers} title="alle getallen"/>
+            <Numbers numbers={numbers.filter((getal) => getal > 6)} title="alle getallen"/>
         </div>
     );
 }
