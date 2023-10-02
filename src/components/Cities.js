ad1eea8d644cf1abbda5ada77802f83279ecdf81
axelt <r0784549@student.thomasmore.be>
2023-10-02 23:11:42 +0200

    5.6 Cities Components

---
+++ src/components/Cities.js
@@ -0,0 +1,37 @@
+import {Col} from "react-bootstrap";
+import {MyCard} from "./MyCard";
+import {Section} from "./Section";
+import PropTypes from "prop-types";
+
+
+function City(props) {
+    const {city} = props;
+    return (
+        <Col xs={12} sm={6} md={4} lg={3} xl={2}>
+            <MyCard title={city.name}>
+                {city.numberOfPersons}
+            </MyCard>
+        </Col>
+    )
+}
+
+City.propTypes = {
+    city: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        numberOfPersons: PropTypes.number.isRequired
+    })
+}
+
+export function Cities(props) {
+    const {cities, title} = props;
+    return (
+        <Section title={title}>
+            {cities.map(c => <City key={c.name} city={c}/>)}
+        </Section>
+    );
+}
+
+Cities.propTypes = {
+    cities: PropTypes.array,
+    title: PropTypes.string
+};
--- src/pages/PersonsPage.js
+++ src/pages/PersonsPage.js
@@ -1,5 +1,7 @@
 import {Persons} from "../components/Persons";
 import {Numbers} from "../components/Numbers";
+import {Cities} from "../components/Cities";
+import {citiesFromPersonData} from "../utilities/person_utilities";
 
 export function PersonsPage(props) {
     const {persons} = props;
@@ -9,6 +11,8 @@ export function PersonsPage(props) {
             <Persons persons={[...persons].sort((p1,p2) => p2.age - p1.age)} title={"alle personen volgens leeftijd"}/>
             <Numbers numbers={persons.map(p => p.age)} title={"leeftijden"}/>
             <Numbers numbers={[...new Set(persons.map(p => p.age))].sort((n1,n2) => n2 - n1)} title={"unieke leeftijden gesorteerd"}/>
+            <Cities cities={[{name: "Antwerpen", numberOfPersons: 100}, {name: "Brussel", numberOfPersons: 200}]} title={"city test"}/>
+            <Cities cities={citiesFromPersonData(persons)} title={"steden van alle personen"}/>
         </div>
     )
 }
