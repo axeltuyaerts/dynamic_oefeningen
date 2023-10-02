0aa23f580a6b8c360381dc50fef90d0bc2ce79ae
axelt <r0784549@student.thomasmore.be>
2023-10-02 22:41:15 +0200

    5.5 show Persons Ages ordered

--- src/pages/PersonsPage.js
+++ src/pages/PersonsPage.js
@@ -8,6 +8,7 @@ export function PersonsPage(props) {
             <Persons persons={persons} title={"alle personen"}/>
             <Persons persons={[...persons].sort((p1,p2) => p2.age - p1.age)} title={"alle personen volgens leeftijd"}/>
             <Numbers numbers={persons.map(p => p.age)} title={"leeftijden"}/>
+            <Numbers numbers={[...new Set(persons.map(p => p.age))].sort((n1,n2) => n2 - n1)} title={"unieke leeftijden gesorteerd"}/>
         </div>
     )
 }
