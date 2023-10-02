8acfc87f853683bedb19b454988d9a12431f10da
axelt <r0784549@student.thomasmore.be>
2023-10-02 22:36:26 +0200

    5.4 show Persons Ages

--- src/pages/PersonsPage.js
+++ src/pages/PersonsPage.js
@@ -1,4 +1,5 @@
 import {Persons} from "../components/Persons";
+import {Numbers} from "../components/Numbers";
 
 export function PersonsPage(props) {
     const {persons} = props;
@@ -6,6 +7,7 @@ export function PersonsPage(props) {
         <div>
             <Persons persons={persons} title={"alle personen"}/>
             <Persons persons={[...persons].sort((p1,p2) => p2.age - p1.age)} title={"alle personen volgens leeftijd"}/>
+            <Numbers numbers={persons.map(p => p.age)} title={"leeftijden"}/>
         </div>
     )
 }
