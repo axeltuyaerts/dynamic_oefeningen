1d08949f868fbab87c991fdf87b11ae61052c13a
axelt <r0784549@student.thomasmore.be>
2023-10-02 20:01:02 +0200

    carColor aangepast

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -12,32 +12,43 @@ const COLOR_DATA = [
     {color: "groen", css: "green", cssFront: "white"}
 ];
 
+function styleColor(color) {
+    const colorCar = COLOR_DATA.find(c => c.color === color);
+    if (!colorCar) return;
+    return {
+        backgroundColor: colorCar.css,
+        color: colorCar.cssFront
+    };
+}
+
+function CarColor(props) {
+    const {color} = props;
+
+    if (!color) return null;
+    return <div style={styleColor(color)}>
+        kleur : {color} &nbsp;
+    </div>
+}
+
 export function Cars(props) {
     const {cars, title} = props;
     return (
 
         <Section title={title}>
-                 {cars.map((car) =>
-                     <Col key={car.id} xs={12} sm={6} md={3} xxl={2} className={"text-center"}>
-                         <Card className={"m-2 p-2 shadow-sm text-center"}>
-                             <div>
-                                 {car.name && <h3>{car.name}</h3>}
-                                 {car.brand && <div>merk: {car.brand}</div>}
-                                 {car.type && <div>type: {car.type}</div>}
-                                 {car.note && <div>opm: {car.note}</div>}
-                                 {car.color &&
-                                     <div style={{backgroundColor: findCarColor(car.color)}}>kleur: {car.color}</div>}
-                             </div>
-                         </Card>
-                     </Col>)}
+            {cars.map((car) =>
+                <Col key={car.id} xs={12} sm={6} md={3} xxl={2} className={"text-center"}>
+                    <Card className={"m-2 p-2 shadow-sm text-center"}>
+                        <div>
+                            {car.name && <h3>{car.name}</h3>}
+                            {car.brand && <div>merk: {car.brand}</div>}
+                            {car.type && <div>type: {car.type}</div>}
+                            {car.note && <div>opm: {car.note}</div>}
+                            <CarColor color={car.color}/>
+                        </div>
+                    </Card>
+                </Col>)}
         </Section>
     );
-}
-
-export function findCarColor(carColor) {
-    const carcolor = carColor;
-    const bgColor = COLOR_DATA.find(carEl => carEl.color === carcolor)
-    return bgColor.css;
 }
 
 Cars.propTypes = {
