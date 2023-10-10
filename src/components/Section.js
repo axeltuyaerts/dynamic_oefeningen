1cf45f5b8686c49008627b613bd5797632eb02f4
axelt <r0784549@student.thomasmore.be>
2023-10-10 21:20:36 +0200

    3.7 tweede Counter

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,7 +6,7 @@ export function Section(props) {
     return (
         <div className={"mt-3 rounded shadow-sm p-2"} style={{backgroundColor: "lavender"}}>
             <h2 className="text-center">{title} <Button className={"bg-transparent text-primary"} style={{width: "5%", margin: "2%"}} onClick={() => alert('tik tak tok')} >klik</Button></h2>
-            <Container>
+            <Container >
                 <Row>
                     {children}
                 </Row>
--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {Section} from "./Section";
 import {MyButton} from "./MyButton";
+import {Counter} from "./Counter";
 
 export function StateDemo() {
     const [demoValue, setDemoValue] = useState(0);
@@ -11,6 +12,13 @@ export function StateDemo() {
             <MyButton onClick={() => setDemoValue(0)}> SET 0 </MyButton>
             <MyButton onClick={() => setDemoValue(77)}> SET 77 </MyButton>
             <MyButton onClick={() => setDemoValue(184)}> SET 184 </MyButton>
+        </div>
+
+        <div>
+            <Counter name={"counter1"}></Counter>
+        </div>
+        <div>
+            <Counter name={"counter2"}></Counter>
         </div>
     </Section>;
 }
