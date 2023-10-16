d74d5e89c88e326254d69ca986ba867ee0645786
Axel <r0784549@student.thomasmore.be>
2023-10-16 16:20:09 +0200

    3.6 Search persons

--- src/pages/PersonsFromDbPage.js
+++ src/pages/PersonsFromDbPage.js
@@ -2,25 +2,32 @@ import {collection} from 'firebase/firestore';
 import {firestoreDB} from "../services/firebase";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import {Persons} from "../components/Persons";
-import React from "react";
+import React, {useState} from "react";
+import {Form} from "react-bootstrap";
 
 const personConverter = {
     toFirestore: undefined,
     fromFirestore: function (snapshot, options) {
         const data = snapshot.data(options);
-        return{...data, id: snapshot.id}
+        return {...data, id: snapshot.id}
     }
 };
 
 export function PersonsFromDbPage() {
-    const collectionRef = collection(firestoreDB,'persons').withConverter(personConverter);
-
+    const collectionRef = collection(firestoreDB, 'persons').withConverter(personConverter);
     const [values, loading, error] = useCollectionData(collectionRef);
+    const [search, setSearch] = useState("");
+
     console.log({values,loading,error});
-    return(
+    return (
         <>
-            <div>Persons from db</div>
-            <Persons persons={values} title={"personen uit de db"} isInitiallyOpen={true}/>
+            <Form>
+                <Form.Label>search</Form.Label>
+                <Form.Control value={search}
+                              onChange={e => setSearch(e.target.value)}/>
+            </Form>
+            <Persons persons={values?.filter((p => p.name.includes(search) || p.city.includes(search)))}
+                     title={"personen uit de db"} isInitiallyOpen={true}/>
         </>
     )
 }
