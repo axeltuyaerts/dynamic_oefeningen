bb232f7ccb24caeadafaa8f86ab46108bf060bab
axelt <r0784549@student.thomasmore.be>
2023-11-07 13:52:30 +0100

    3.3 more buttons

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -7,6 +7,8 @@ export function StateDemo() {
 
     return <Section title={"state"}>
         <div>de waarde van de demoValue is {demoValue}</div>
-        <Button onClick={() => setDemoValue(77)}> SET 77</Button>
+        <Button className={"m-3"} onClick={() => setDemoValue(0)}> SET 0</Button>
+        <Button className={"m-3"}  onClick={() => setDemoValue(77)}> SET 77</Button>
+        <Button className={"m-3"}  onClick={() => setDemoValue(184)}> SET 184</Button>
     </Section>
 }
