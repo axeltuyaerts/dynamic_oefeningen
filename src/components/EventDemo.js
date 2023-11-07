c9d1ef0595084fc1ef9a2d3e821cc30e9a137be7
axelt <r0784549@student.thomasmore.be>
2023-11-07 10:51:57 +0100

    2.6 & 2.7 preventDefault and stopPropagation

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -8,7 +8,17 @@ export function EventDemo() {
                 alert("Button is clicked")
             }}>click me please!
             </button>
-            <Button className={"m-3"} onClick={e => alert(`bootstrap is clicked - pos : ${e.clientX}, ${e.clientY}`)}>click me please!</Button>
+            <Button className={"m-3"} onClick={e => alert(`bootstrap is clicked - pos : ${e.clientX}, ${e.clientY}`)}>click
+                me please!</Button>
+
+            <div className={"m-3"} style={{backgroundColor: "pink"}} onClick={() => alert("div is clicked")}>
+                <div>dit is een div met eventHandler</div>
+                <a href="http://google.com" onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    alert("link is clicked ")
+                }}>dit is een link met een href</a> - en - <Button onClick={(e) => {e.stopPropagation(); alert("button is clicked")}}>dit is een button</Button>
+            </div>
         </Section>
 
     );
