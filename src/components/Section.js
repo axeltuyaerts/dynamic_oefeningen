c40c63e23f755fc519d594fed1c0eb66acf4d3ff
axelt <r0784549@student.thomasmore.be>
2023-11-07 18:01:03 +0100

    4.4 open/close Section Component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,38 @@
 import PropTypes from "prop-types";
 import {Button, Container, Row} from "react-bootstrap";
+import {useState} from "react";
+
+function OpenCloseButton(props) {
+    const {isOpen, onChangeIsOpen} = props;
+
+    return (
+        <Button variant="outline-primary" size="sm"
+                className="ms-4"
+                onClick={() => onChangeIsOpen(!isOpen)}>
+            {isOpen ? "-" : "+"}
+        </Button>
+    )
+}
+
+function SectionContent(props) {
+    const {isOpen, children} = props;
+    if (!isOpen) return;
+    return (
+        <Container>
+            <Row>
+                {children}
+            </Row>
+        </Container>
+    )
+}
 
 export function Section(props) {
     const {title, children} = props;
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={"mt-3 rounded shadow-sm"} style={{backgroundColor: "lavender"}}>
-            <h2 className="text-center">{title} <Button onClick={() => alert("klik klik klik")}>klik</Button></h2>
-            <Container>
-                <Row>
-                    {children}
-                </Row>
-            </Container>
+            <h2 className="text-center">{title} <OpenCloseButton isOpen={isOpen} onChangeIsOpen={() => setIsOpen(!isOpen)}/></h2>
+            <SectionContent isOpen={isOpen}>{children}</SectionContent>
         </div>
     );
 }
