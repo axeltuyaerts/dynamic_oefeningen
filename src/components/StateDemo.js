d17f9c888bfb0354af25528276487062cb9d45f9
axelt <r0784549@student.thomasmore.be>
2023-11-07 15:10:24 +0100

    4.1 extract component DemoValue

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -4,15 +4,25 @@ import {MyButton} from "./MyButton";
 import {Counter} from "./Counter";
 import {OnOffDemo} from "./OnOffDemo";
 
-export function StateDemo() {
+function DemoValue() {
     const [demoValue, setDemoValue] = useState(0);
 
+    return (
+        <div>
+            <div>de waarde van de demoValue is {demoValue}</div>
+            <div>de waarde van de demoValue is {demoValue}</div>
+            <div>
+                <MyButton onClick={() => setDemoValue(0)}> SET 0</MyButton>
+                <MyButton onClick={() => setDemoValue(77)}> SET 77</MyButton>
+                <MyButton onClick={() => setDemoValue(184)}> SET 184</MyButton>
+            </div>
+        </div>
+    )
+}
+
+export function StateDemo() {
     return <Section title={"state"}>
-        <div>de waarde van de demoValue is {demoValue}</div>
-        <div>de waarde van de demoValue is {demoValue}</div>
-        <MyButton onClick={() => setDemoValue(0)}> SET 0</MyButton>
-        <MyButton onClick={() => setDemoValue(77)}> SET 77</MyButton>
-        <MyButton onClick={() => setDemoValue(184)}> SET 184</MyButton>
+        <DemoValue/>
         <Counter name={"counter1"}/>
         <Counter name={"counter2"}/>
         <OnOffDemo/>
