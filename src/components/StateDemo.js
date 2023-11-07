34e8e513b0271210b2d5b57711b81d2ea30593d5
axelt <r0784549@student.thomasmore.be>
2023-11-07 14:00:25 +0100

    3.4 Component MyButton

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -1,14 +1,14 @@
 import {useState} from "react";
 import {Section} from "./Section";
-import {Button} from "react-bootstrap";
+import {MyButton} from "./MyButton";
 
 export function StateDemo() {
     const [demoValue, setDemoValue] = useState(0);
 
     return <Section title={"state"}>
         <div>de waarde van de demoValue is {demoValue}</div>
-        <Button className={"m-3"} onClick={() => setDemoValue(0)}> SET 0</Button>
-        <Button className={"m-3"}  onClick={() => setDemoValue(77)}> SET 77</Button>
-        <Button className={"m-3"}  onClick={() => setDemoValue(184)}> SET 184</Button>
+        <MyButton onClick={() => setDemoValue(0)}> SET 0</MyButton>
+        <MyButton onClick={() => setDemoValue(77)}> SET 77</MyButton>
+        <MyButton onClick={() => setDemoValue(184)}> SET 184</MyButton>
     </Section>
 }
